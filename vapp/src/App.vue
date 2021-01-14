3ec71722d56dee1bf33e7aa03c7a90472d391244
flairxy <okonkwo.miracle@raadaa.com>
2021-01-14 13:57:23 +0100

    simpleStorage using vuejs + truffle + drizzle

---
+++ vapp/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div v-if="isDrizzleInitialized" id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+
+    <div class="section">
+      <h2>Show the Accounts</h2>
+      <drizzle-account units="Ether" :precision="2" />
+    </div>
+
+    <!-- <div class="section">
+      <h2>Tutorial Token</h2>
+      <TutorialToken />
+    </div> -->
+
+    <div class="section">
+      <h2>Simple Storage</h2>
+      <SimpleStorage />
+    </div>
+
+    <div class="section">
+      <Toast />
+    </div>
+
+    <!-- <div class="section">
+      <h2>Complex Storage</h2>
+      <ComplexStorage />
+    </div> -->
+  </div>
+
+  <div v-else>Loading...</div>
+</template>
+
+<script>
+// import TutorialToken from "./TutorialToken";
+// import ComplexStorage from "./ComplexStorage";
+import SimpleStorage from "./SimpleStorage";
+import Toast from "./Toast";
+import { mapGetters } from "vuex";
+
+export default {
+  name: "app",
+  components: {
+    // ComplexStorage,
+    // TutorialToken,
+    SimpleStorage,
+    Toast,
+  },
+
+  computed: mapGetters("drizzle", ["isDrizzleInitialized"]),
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vapp/src/Toast.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+<script>
+import Toasted from "vue-toasted";
+import Vue from "vue";
+Vue.use(Toasted);
+export default {
+  mounted() {
+    const contractEventHandler = ({ contractName, eventName, data }) => {
+      const display = `${contractName}(${eventName}): ${data._message}}`;
+      // const display = `${contractName}(${eventName}): ${data.newValue}, ${data.date}`;
+      const subOption = { duration: 3000 };
+      this.$toasted.show(display, subOption);
+    };
+    this.$drizzleEvents.$on("drizzle/contractEvent", (payload) => {
+      contractEventHandler(payload);
+    });
+  },
+};
+</script>
